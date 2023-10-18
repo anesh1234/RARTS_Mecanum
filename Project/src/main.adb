49654abb1467c4747700d57f74fa2fddc14fb057
Stian N <stiannordholm@gmail.com>
2023-10-18 13:29:05 +0200

    updated main

--- Project/src/main.adb
+++ Project/src/main.adb
@@ -6,30 +6,15 @@ with MicroBit.Console; use MicroBit.Console; -- for serial port communication
 use MicroBit; --for pin names
 
 procedure Main is
-
 begin
-   MotorDriver.Servo(1,90);
-   delay 1.0; -- equivalent of Time.Sleep(1000) = 1 second
-
    loop
-      --   DEMONSTRATION ROUTINE 4 MOTORS (useful for checking your wiring)
-      --  MotorDriver.Drive(Forward,(4095,0,0,0)); --right front wheel to M4
-      --  delay 1.0;
-      --  MotorDriver.Drive(Forward,(0,4095,0,0)); --right back wheel to M3
-      --  delay 1.0;
-      --  MotorDriver.Drive(Forward,(0,0,4095,0)); --left front wheel to M2
-      --  delay 1.0;
-      --  MotorDriver.Drive(Forward,(0,0,0,4095)); --left back wheel to M1
-      --  delay 1.0;
-      --  MotorDriver.Drive(Stop);
-
-
-      MotorDriver.Drive(Forward);
+      MotorDriver.Drive(Forward_Left);
       delay 1.0;
+      MotorDriver.Drive(Forward_Right);
+      delay 1.0;
+
       MotorDriver.Drive(stop);
-
-      delay 3.0;
-
+      delay 4.0;
 
    end loop;
 end Main;

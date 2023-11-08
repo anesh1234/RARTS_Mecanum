b43b452b761c097d55554b74db42d4483d20cf00
Stian N <stiannordholm@gmail.com>
2023-11-08 14:42:56 +0100

    Added support for diagonal driving

--- Remote Microbit/src/main.adb
+++ Remote Microbit/src/main.adb
@@ -1,34 +1,36 @@
-with LSM303AGR; use LSM303AGR;
+with LSM303AGR;                  use LSM303AGR;
 with MicroBit.Radio;
-with HAL; use HAL;
-with MicroBit.Console; use MicroBit.Console;
-with MicroBit.Buttons; use MicroBit.Buttons;
+with HAL;                        use HAL;
+with MicroBit.Console;           use MicroBit.Console;
+with MicroBit.Buttons;           use MicroBit.Buttons;
 with MicroBit.Accelerometer;
 with MicroBit.DisplayRT;
-with MicroBit.DisplayRT.Symbols;
-use MicroBit;
+with MicroBit.DisplayRT.Symbols; use MicroBit;
 procedure Main is
    RXdata : Radio.RadioData;
    TxData : Radio.RadioData;
 
    --Defining accelData and threshold for acceleromenter
-   accelData: All_Axes_Data;
+   accelData      : All_Axes_Data;
    accelThreshold : constant := 150;
 begin
-   TxData.Length := 3+2; -- This is important! Header is 4-1=3, payload is 2 bytes. If higher payload needed change this to a max of 32.
-   TxData.Version:= 12;  -- Since CRC is a bit buggy (see Radio package), current implementation uses both length and version as additional hardcoded crc check so make sure they match in both sender and receiver
-   TxData.Group := 201;    -- Since messages are broadcasted, a group can be used as unique ID to only use message of a certain ID
-   TxData.Protocol := 14;-- Protocol can be used for setting up a handshake for example. This could be useful to confirm if a package with message ID is actually received by sending an acknowledgement to the Sender. The Sender can then stop with sending message that messageID and send the next package.
+   TxData.Length   :=
+     3 +
+     2; -- This is important! Header is 4-1=3, payload is 2 bytes. If higher payload needed change this to a max of 32.
+   TxData.Version  :=
+     12;  -- Since CRC is a bit buggy (see Radio package), current implementation uses both length and version as additional hardcoded crc check so make sure they match in both sender and receiver
+   TxData.Group    :=
+     201;    -- Since messages are broadcasted, a group can be used as unique ID to only use message of a certain ID
+   TxData.Protocol :=
+     14;-- Protocol can be used for setting up a handshake for example. This could be useful to confirm if a package with message ID is actually received by sending an acknowledgement to the Sender. The Sender can then stop with sending message that messageID and send the next package.
 
-   Radio.Setup(RadioFrequency => 2407,
-               Length => TxData.Length,
-               Version => TxData.Version,
-               Group => TxData.Group,
-               Protocol => TxData.Protocol);
+   Radio.Setup
+     (RadioFrequency => 2_407, Length => TxData.Length,
+      Version        => TxData.Version, Group => TxData.Group,
+      Protocol       => TxData.Protocol);
 
    Radio.StartReceiving;
-   Put_Line(Radio.State); -- this should report Status: 3, meaning in RX mode
-
+   Put_Line (Radio.State); -- this should report Status: 3, meaning in RX mode
 
    loop
       --Read accel data
@@ -37,35 +39,66 @@ begin
       --Logic for the accelerometer
       MicroBit.DisplayRT.Clear;
 
-      if MicroBit.Buttons.State (Button_A) = Pressed and MicroBit.Buttons.State (Button_B) = Pressed then
-         TransmitData(TxData, 1);
+      if MicroBit.Buttons.State (Button_A) = Pressed and
+        MicroBit.Buttons.State (Button_B) = Pressed
+      then
+         TransmitData (TxData, 1);
+
       elsif accelData.X > accelThreshold then
-         MicroBit.DisplayRT.Symbols.Left_Arrow;
-         TransmitData(TxData, 5);
+         if accelData.Y > accelThreshold then
+            --Dia FL
+            TransmitData (TxData, 7);
+         else
+            MicroBit.DisplayRT.Symbols.Left_Arrow;
+            TransmitData (TxData, 5);
+         end if;
+
       elsif accelData.X < -accelThreshold then
-         MicroBit.DisplayRT.Symbols.Right_Arrow;
-         TransmitData(TxData, 6);
+         if accelData.Y < -accelThreshold then
+            --Dia BR
+            TransmitData (TxData, 10);
+         else
+            MicroBit.DisplayRT.Symbols.Right_Arrow;
+            TransmitData (TxData, 6);
+         end if;
+
       elsif accelData.Y > accelThreshold then
+         if accelData.X < -accelThreshold then
+            --Dia FR
+            TransmitData (TxData, 8);
+         else
+            MicroBit.DisplayRT.Symbols.Left_Arrow;
+            TransmitData (TxData, 5);
+         end if;
          MicroBit.DisplayRT.Symbols.Up_Arrow;
-         TransmitData(TxData, 3);
+         TransmitData (TxData, 3);
+
       elsif accelData.Y < -accelThreshold then
+         if accelData.X > accelThreshold then
+            --Dia BL
+            TransmitData (TxData, 9);
+         else
+            MicroBit.DisplayRT.Symbols.Right_Arrow;
+            TransmitData (TxData, 6);
+         end if;
          MicroBit.DisplayRT.Symbols.Down_Arrow;
-         TransmitData(TxData, 4);
+         TransmitData (TxData, 4);
+
       else
          MicroBit.DisplayRT.Symbols.Heart;
+         TransmitData (TxData, 11);
 
       end if;
       --End of logic for accelerometer
 
       -- repeat every 500ms
-      delay(0.5);
+      delay (0.5);
 
    end loop;
 end Main;
 
-
 procedure TransmitData (TXdata : Radio.RadioData; Data : UInt8) is
 begin
-   TxData.Payload(1) := Data;
-   Radio.Transmit(TXdata);
+   TxData.Payload (1) := Data;
+   Radio.Transmit (TXdata);
 end TransmitData;
